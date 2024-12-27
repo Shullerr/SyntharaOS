7281e93a5a5f3035bb8041d5bf72c56b1974ac46
Shullerr <ghenadie.grapin@gmail.com>
2024-12-27 23:07:19 +0100

    Create memory_management.c

---
+++ src/kernel/memory_management.c
@@ -0,0 +1,9 @@
+// Simple memory management
+#include "common.h"
+
+void init_memory_manager() {
+    // Set up memory management structures, e.g., page tables or simple heap
+    // This is a very basic example - more complex memory management will follow
+    // in later stages (e.g., paging, virtual memory).
+    terminal_writestring("Memory Manager Initialized.\n");
+}
